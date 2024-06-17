996b8b9099ca655dc7ab337e28618ea0db8db73b
jvav-runtime-environment <dannybxx@qq.com>
2024-06-17 17:20:25 +0800

    new file:   utils/connection/connections.py

---
+++ utils/connection/connections.py
@@ -0,0 +1,18 @@
+import connection
+import threading
+
+class ConnectionPool():
+    def __init__(self):
+        self.connections = []
+
+    def set_connection_num(self, num):
+        pass
+
+    def send(self, msg):
+        pass
+
+    def recv(self):
+        pass
+
+    def __thread_func():
+        pass
